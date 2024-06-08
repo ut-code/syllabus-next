cb8a00f69bff53626bbb673fe36edd4e99890c9c
Rei Tsukada <rayzou1120@icloud.com>
2024-06-08 13:03:31 +0900

    localstorageで管理する登録情報の管理

---
+++ src/app/type.ts
@@ -0,0 +1,12 @@
+/**
+ * @typedef {RegisterType}
+ * @property {string | undefined} stream 科類
+ * @property {string | undefined} grade 学年
+ * @property {string | undefined} className 組
+ */
+export type RegisterType = {
+  stream: string | undefined;
+  grade: string | undefined;
+  classNumber: string | undefined;
+};
+
