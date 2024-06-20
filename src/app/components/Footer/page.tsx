dd52d8df8574b00753fd49c1fe5e1b793387256f
Rei Tsukada <rayzou1120@icloud.com>
2024-06-20 19:11:53 +0900

    色の変更

--- src/app/components/Footer/page.tsx
+++ src/app/components/Footer/page.tsx
@@ -8,42 +8,42 @@ export default function Footer() {
   return (
     <footer
       id="global-footer"
-      className="flex justify-end items-center p-4 text-white fixed bottom-0 w-full"
+      className="flex justify-end items-center p-4 text-white fixed bottom-0 w-full bg-inverse-primary"
     >
-      <div className="flex flex-wrap justify-end space-x-4">
+      <div className="flex flex-wrap justify-end space-x-4 bg-gray-800">
         <Link
           href="https://forms.gle/stBdKuCsXm8LUdBm8"
-          className="hover:bg-gray-700 hover:text-white px-2 py-1 rounded transition duration-200"
+          className="hover:bg-surface-variant hover:text-white px-2 py-1 rounded transition duration-200 "
         >
           ご意見
         </Link>
         <Link
           href="howToUse.html"
-          className="hover:bg-gray-700 hover:text-white px-2 py-1 rounded transition duration-200"
+          className="hover:bg-surface-variant hover:text-white px-2 py-1 rounded transition duration-200"
         >
           使い方
         </Link>
         <Link
           href="notion.html"
-          className="hover:bg-gray-700 hover:text-white px-2 py-1 rounded transition duration-200"
+          className="hover:bg-surface-variant hover:text-white px-2 py-1 rounded transition duration-200"
         >
           履修上の注意
         </Link>
         <Link
           href="disclaimer.html"
-          className="hover:bg-gray-700 hover:text-white px-2 py-1 rounded transition duration-200"
+          className="hover:bg-surface-variant hover:text-white px-2 py-1 rounded transition duration-200"
         >
           免責事項
         </Link>
         <Link
           href="aboutus.html"
-          className="hover:bg-gray-700 hover:text-white px-2 py-1 rounded transition duration-200"
+          className="hover:bg-surface-variant hover:text-white px-2 py-1 rounded transition duration-200"
         >
           About us
         </Link>
         <Link
           href="https://utcode.net/"
-          className="hover:bg-gray-700 hover:text-white px-2 py-1 rounded transition duration-200"
+          className="hover:bg-surface-variant hover:text-white px-2 py-1 rounded transition duration-200"
           target="_blank"
         >
           ut.code();
