c82bce92b600a2929315a72693ece7702ce8eaa0
Axafy <mirochacha_u_u_1106@yahoo.co.jp>
2024-07-28 16:17:37 +0800

    [Add] title design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import Header from "@/app/components/Header";
+import Logo from "@/../../public/syllabus_icon.svg"
+import Image from "next/image";
 /**
  * Home コンポーネントは、ホームページの内容を表示します。
  * @returns HTMLを生成するReactコンポーネント。
@@ -7,6 +9,21 @@ export default function Home(): JSX.Element {
   return (
     <div>
       <Header />
+      <div className="flex items-center min-h-screen ml-24">
+        <img className="absolute right-0 h-[140%] opacity-40" src={"/backgroundImage.png"} alt="backgroundImage" />
+        <div className="justify-center items-center">
+          <div className="relative flex items-center rounded-lg z-10">
+            <Image src={Logo} alt="Logo" width={150} height={150} className="mr-4" />
+            <div className="flex items-center">
+              <div className="m-4">
+                <h1 className="text-6xl font-bold mb-12">Welcome to シ楽バス</h1>
+                <p className="text-2xl">全ての講義情報を、いつも手元に。</p>
+              </div>
+            </div>
+          </div>
+          <div className="rounded-lg bg-primary text-on-primary w-36 text-center p-4 mt-24 hover:bg-primary/90">はじめる</div>
+        </div>
+      </div>
     </div>
   );
 }
