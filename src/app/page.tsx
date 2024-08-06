ef549cfe4ea2c78d992c81642253897714639565
Axafy <mirochacha_u_u_1106@yahoo.co.jp>
2024-08-06 17:24:57 +0800

    [modify] designed logo [add] favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,6 @@ import Header from "@/app/components/Header";
 import Logo from "@/../../public/syllabus_icon.svg";
 import Image from "next/image";
 
-const logoSize = "150px";
-const logoSizeS = "100px";
-const backgroundImageWidth = "800px";
-const titleFontSize = "6xl";
-const subtitleFontSize = "2xl";
-
 /**
  * Home コンポーネントは、ホームページの内容を表示します。
  * @returns HTMLを生成するReactコンポーネント。
@@ -18,9 +12,7 @@ export default function Home(): JSX.Element {
       <Header />
 
       {/* バックグラウンド画像 */}
-      <div
-        className={`absolute right-0 bottom-0 w-[${backgroundImageWidth}] h-full`}
-      >
+      <div className={`absolute right-0 bottom-0 w-[800px] h-full`}>
         <Image
           className="opacity-40 object-cover"
           src={"/backgroundImage.png"}
@@ -36,7 +28,7 @@ export default function Home(): JSX.Element {
           <div className="relative flex justify-center items-center rounded-lg z-10">
             {/* ロゴ(解像度に応じてサイズを変える) */}
             <div
-              className={`relative w-[${logoSizeS}] h-[${logoSizeS}] md:w-[${logoSize}] md:h-[${logoSize}]`}
+              className={`relative w-[100px] h-[100px] md:w-[150px] md:h-[150px]`}
             >
               <Image
                 src={Logo}
@@ -49,16 +41,14 @@ export default function Home(): JSX.Element {
             {/* タイトル&サブタイトル */}
             <div className="m-4">
               {/* タイトル */}
-              <div
-                className={`text-${titleFontSize} font-bold break-keep text-left`}
-              >
+              <div className={`text-6xl font-bold break-keep text-left`}>
                 {/* 幅に余裕があるときに表示 */}
                 <span className="max-md:hidden">Welcome to </span>シ楽バス
               </div>
 
               {/* サブタイトル (幅に余裕があるときに表示) */}
               <p
-                className={`mt-12 text-${subtitleFontSize} text-center md:text-left max-sm:hidden`}
+                className={`mt-12 text-2xl text-center md:text-left max-sm:hidden`}
               >
                 全ての講義情報を、いつも手元に。
               </p>
@@ -67,7 +57,7 @@ export default function Home(): JSX.Element {
 
           {/* はじめるボタン */}
           <div className="flex items-center justify-center">
-            <div className="rounded-lg bg-primary text-on-primary w-36 text-center p-4 mt-24 hover:bg-primary/90 font-bold">
+            <div className="rounded-full bg-primary text-on-primary w-36 text-center p-3 mt-24 hover:bg-primary/90 font-bold">
               はじめる
             </div>
           </div>
