989568bd267e4878bac05d91b3909be3babebf09
Rei Tsukada <rayzou1120@icloud.com>
2024-10-24 15:36:02 +0900

    個人データ入力画面
    (cherry picked from commit 98a19bbdabbbdf2bf328619e493fd5019e5c0011)

---
+++ src/app/profile/Selector.tsx
@@ -0,0 +1,39 @@
+type Option = {
+  value: string;
+  data: string;
+};
+
+export type SelectorProps = {
+  options: Option[];
+  value: string;
+};
+/**
+ * Selector ドロップダウンリストを表示するコンポーネント。
+ * @param props options: Option[] ドロップダウンリストの選択肢。
+ * @returns HTMLを生成するReactコンポーネント。
+ */
+export default function Selector(props: SelectorProps): JSX.Element {
+  const { options, value } = props;
+
+  return (
+    <form className="max-w-sm mx-auto">
+      <label
+        htmlFor="countries"
+        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+      ></label>
+      <select
+        id="countries"
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      >
+        <option disabled selected>
+          {value}
+        </option>
+        {options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.data}
+          </option>
+        ))}
+      </select>
+    </form>
+  );
+}
