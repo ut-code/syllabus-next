57d0c88e5e344cfd4106a5c6af92f0a8d45bc33d
Rei Tsukada <rayzou1120@icloud.com>
2025-01-05 20:45:28 +0900

    fix on server

--- src/app/utils/user.ts
+++ src/app/utils/user.ts
@@ -9,11 +9,15 @@ export class User {
   private user: RegisterType | undefined;
 
   constructor() {
-    if (MODE === "mock") {
-      this.user = SampleUser;
+    if (typeof window !== "undefined") {
+      if (MODE === "mock") {
+        this.user = SampleUser;
+      } else {
+        const storedUser = localStorage.getItem("user");
+        this.user = storedUser ? JSON.parse(storedUser) : undefined;
+      }
     } else {
-      const storedUser = localStorage.getItem("user");
-      this.user = storedUser ? JSON.parse(storedUser) : undefined;
+      this.user = undefined;
     }
   }
 
@@ -28,12 +32,13 @@ export class User {
 
   /**
    * ユーザー情報を更新します。
+   * クライアントサイドのみで localStorage を更新します。
    * @param newUser 新しいユーザー情報
    */
   setUser(newUser: RegisterType): void {
     this.user = newUser;
 
-    if (MODE !== "mock") {
+    if (typeof window !== "undefined" && MODE !== "mock") {
       localStorage.setItem("user", JSON.stringify(newUser));
     }
   }
