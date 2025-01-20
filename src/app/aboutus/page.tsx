531e7e51ccf476f0bd5f3f6d14968efc03280552
RRRyoma <119156952+RRRyoma@users.noreply.github.com>
2025-01-19 20:57:16 -0800

    prettier formatted

--- src/app/aboutus/page.tsx
+++ src/app/aboutus/page.tsx
@@ -9,12 +9,13 @@ const AboutUs: React.FC = () => {
   return (
     <>
       <HowToUseItem title="About us">
-        私たち ut.code(); は、2019年度に発足した東京大学のプログラミングサークルです。主にWeb関連の技術を用い、私たちの生活を彩るプログラムを開発することを目標としています。
-        <br/>
+        私たち ut.code();
+        は、2019年度に発足した東京大学のプログラミングサークルです。主にWeb関連の技術を用い、私たちの生活を彩るプログラムを開発することを目標としています。
+        <br />
         <a href="https://line.me/R/ti/p/%40nlr3843e">LINE add friend</a>
-        <br/>
+        <br />
         <a href="https://twitter.com/utokyo_code">X(旧Twitter)</a>
-        <br/>
+        <br />
         <a href="https://utcode.net">Webサイト</a>
       </HowToUseItem>
     </>
