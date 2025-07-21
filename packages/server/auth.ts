24640f3a6142714de0d7560c1f8107d040ed711b
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2025-07-21 13:29:54 +0900

    use google oauth

--- packages/server/auth.ts
+++ packages/server/auth.ts
@@ -6,7 +6,10 @@ export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "sqlite",
   }),
-  emailAndPassword: {
-    enabled: true,
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
   },
 });
