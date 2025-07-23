352583d46a250a39f2c0d607f9bfd8244ca682ef
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2025-07-23 20:08:29 +0900

    rename table name

--- packages/server/db/schema.ts
+++ packages/server/db/schema.ts
@@ -1,7 +1,7 @@
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 // sample table
-export const users = sqliteTable("users_table", {
+export const users = sqliteTable("users", {
   id: integer().primaryKey({ autoIncrement: true }),
   name: text().notNull(),
   age: integer().notNull(),
