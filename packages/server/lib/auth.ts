0fdc747bff3f82d364b4a2a95a12c68ff8079816
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2025-07-23 19:43:25 +0900

    delete select_account prompt

--- packages/server/lib/auth.ts
+++ packages/server/lib/auth.ts
@@ -1,7 +1,7 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { db } from "./db/index.ts";
-import * as schema from "./db/schema.ts";
+import { db } from "../db/index.ts";
+import * as schema from "../db/schema.ts";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
@@ -10,7 +10,6 @@ export const auth = betterAuth({
   }),
   socialProviders: {
     google: {
-      prompt: "select_account",
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     },
